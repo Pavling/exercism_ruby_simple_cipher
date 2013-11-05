0fb4d50da0cabd0a1de846e6f1d5391f5d9fc93d
Michael Pavling <pavling@gmail.com>
2013-11-05 13:11:30 +0000

    passing encode test

--- cipher.rb
+++ cipher.rb
@@ -8,6 +8,12 @@ class Cipher
     @key ||= generate_random_key
   end
 
+  def encode(plaintext)
+    plaintext.chars.each_with_index.map do |char, index|
+      (((key[index % key.length].ord - 97) % 26) + char.ord).chr
+    end.join
+  end
+
   private
   def generate_random_key
     (0...100).map { (97 + rand(26)).chr }.join
