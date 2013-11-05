e6d027830d129e167ce5f89ad41bd480f0fd09d1
Michael Pavling <pavling@gmail.com>
2013-11-05 13:13:10 +0000

    passing decode test

--- cipher.rb
+++ cipher.rb
@@ -14,6 +14,12 @@ class Cipher
     end.join
   end
 
+  def decode(ciphertext)
+    ciphertext.chars.each_with_index.map do |char, index|
+      (char.ord - ((key[index % key.length].ord - 97) % 26)).chr
+    end.join
+  end
+
   private
   def generate_random_key
     (0...100).map { (97 + rand(26)).chr }.join
--- simple-cipher_test.rb
+++ simple-cipher_test.rb
@@ -19,7 +19,6 @@ class RandomKeyCipherTest < MiniTest::Unit::TestCase
   end
 
   def test_cipher_decode
-    skip
     plaintext = "aaaaaaaaaa"
     assert_equal(plaintext, @cipher.decode(@cipher.key[0,10]))
   end
